828a91b94599e31caa71b3d0e811e761254a5418
Giang Son <giangson1311@gmail.com>
2024-11-23 15:41:31 +1030

    final css

--- src/services/app.module.ts
+++ src/services/app.module.ts
@@ -9,22 +9,31 @@ import { QueService } from './que.service';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from 'src/app/app/app.component';
 import { RouterOutlet } from '@angular/router';
-import { MatDatepickerModule} from '@angular/material/datepicker';
-import { MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SinhKhacComponent } from 'src/app/sinh-khac/sinh-khac.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    QueComponent
-  ],
+  declarations: [AppComponent, HomeComponent, QueComponent, SinhKhacComponent],
   imports: [
-    BrowserModule,FormsModule, HttpClientModule, AppRoutingModule, RouterModule, RouterOutlet,
-    MatDatepickerModule, MatInputModule, MatNativeDateModule, BrowserAnimationsModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule,
+    RouterOutlet,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
   ],
-  providers: [QueService],
-  bootstrap: [AppComponent]
+  providers: [
+    QueService,
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/services/que.service.ts
+++ src/services/que.service.ts
@@ -4,19 +4,19 @@ import { QueDTO } from 'src/dtos/que.dto';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class QueService {
-
-  private apiUrl = `http://localhost:8088/api/v1/kinhdich/que`;
+  // private apiUrl1 = `http://localhost:8088/api/v1/kinhdich/que`;     // 8088 - connect local host via Xammp
+  // private apiUrl = `http://localhost:8099/api/v1/kinhdich/que`;      // 8099 - connect via Docker Container
+  private apiUrl = `http://103.101.163.117:8099/api/v1/kinhdich/que`; // 8099 - connect via server VPS
 
   constructor(private http: HttpClient) {}
 
   // Ham submit Que trong service
   checkQue(QueDTO: QueDTO): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    
+
     return this.http.post(this.apiUrl, QueDTO, { headers });
   }
 }
